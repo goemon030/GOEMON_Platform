03712d2029d94b2c0cad58ba38dae5be595d7cf9
goemon030 <goemon.ceo@gmail.com>
2026-06-14 04:37:53 +0900

    Enabled CSRF Security guard

---
+++ src/main/java/com/goemon/portfolio/config/SecurityConfig.java
@@ -0,0 +1,28 @@
+package com.goemon.portfolio.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    SecurityFilterChain securityFilterChain(
+            HttpSecurity http
+    ) throws Exception {
+
+        http.authorizeHttpRequests(
+                auth -> auth
+                    .anyRequest()
+                    .permitAll()
+            );
+
+        return http.build();
+
+    }
+
+}
