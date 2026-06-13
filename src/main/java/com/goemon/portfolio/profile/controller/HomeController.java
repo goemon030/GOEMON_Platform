9e83d0526f8ccb85a2f63475769ac2125ea6c500
goemon030 <goemon.ceo@gmail.com>
2026-06-14 04:48:11 +0900

    create HomeController

---
+++ src/main/java/com/goemon/portfolio/profile/controller/HomeController.java
@@ -0,0 +1,26 @@
+package com.goemon.portfolio.profile.controller;
+
+import org.springframework.stereotype.Controller;
+
+import org.springframework.ui.Model;
+
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+
+    @GetMapping("/")
+    public String index(
+            Model model
+    ) {
+
+        model.addAttribute(
+                "name",
+                "GOEMON"
+        );
+
+        return "index";
+
+    }
+
+}
